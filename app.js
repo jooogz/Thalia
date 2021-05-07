eaccbcbc9907ba006280b58fa7a746d8410d286d
Jack O'Connell <74830299+jooogz@users.noreply.github.com>
2021-05-07 18:48:50 +0100

    added a cute pet feature

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const Discord = require('discord.js');
 const { getRandomMeme } = require('./randomMeme');
+const { getRandomAww } = require('/.randomAww')
 require('dotenv').config();
 fetch = require('node-fetch')
 const client = new Discord.Client();
@@ -11,5 +12,10 @@ client.on('ready',  () =>  console.log('The Bot is ready!'));
 client.on('message', async msg => {
     if(msg.content === 'get') {
         msg.channel.send(await getRandomMeme());
+    }
+});
+client.on('message', async msg => {
+    if(msg.content === 'aww') {
+        msg.channel.send(await getRandomAww());
     }
 });
---
+++ randomAww.js
@@ -0,0 +1,18 @@
+fetch = require('node-fetch')
+module.exports = {
+    getRandomAww: async function() {
+      let aww = [];
+    await fetch("https://www.reddit.com/r/aww/new.json?limit=100")
+        .then(data => data.json())
+        .then(res => {            
+            const children = res.data.children;
+            for(let i= 0; i < children.length; i++ ){
+                const link = children[i].data.url_overridden_by_dest;
+                if(link) {
+                    aww.push(link)                    
+                }
+            }
+        });
+        return aww[Math.floor(Math.random() * aww.length)]; 
+    }
+}
